c6e223fd6e2cfc9ac05bbdb82250e3c1c250cf40
Robert Johnson <robljohn1998@gmail.com>
2022-12-01 12:26:34 -0500

    add new basic content for index.html, first change to style.css

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
+// TESTING API WITH MY PHONE #
+// const promise = $.ajax({
+//     url:'https://phonevalidation.abstractapi.com/v1/?api_key=ddc43a2fb74b444492d3d4b0da841e8d&phone=15712252970'
+// })
+
+// promise.then(
+//     (data)=>{
+//         console.log(data);
+//     },
+//     (error) => {
+//         console.log('bad request: ', error)
+//     }
+// )
+
+//VARIABLES
+
 const URL = 'https://phonevalidation.abstractapi.com/v1/?api_key=ddc43a2fb74b444492d3d4b0da841e8d&phone=1'
 //Note that &phone=1
